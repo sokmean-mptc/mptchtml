f260ebd3d6082f54b1d31306e48060f2e4cc364e
sokmean-mptc <ngonsokmean@mptc.gov.kh>
2020-02-23 18:21:02 +0700

    update some feathure (feedback and servay interface)

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,7 @@ import 'bootstrap';
 import 'slick-carousel/slick/slick.min.js';
 import './jquery.youtubeplaylist.js';
 import './common.js';
+import servay from './servay'
+import feedback from './feedback';
+window.servay = new servay()
+window.feedback = new feedback()
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 module.exports = {
     mode: 'production',
@@ -46,7 +48,9 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
-        })
+        }),
+        new CleanWebpackPlugin(),
+        new BrowserSyncPlugin()
     ],
     optimization: {
         minimizer: [
